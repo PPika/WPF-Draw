d55a80db596d93e0d216f0ba18171bd961d0686b
haeer <201600732@yangtzeu.edu.cn>
2018-04-16 09:53:48 +0800

    重新构造函数

--- Canvas/MainWindow.xaml.cs
+++ Canvas/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Canvas
+namespace MyCanvas
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
--- Canvas/temp.cs
+++ Canvas/temp.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using Use;
 
-namespace Canvas
+namespace MyCanvas
 {
     public static class temp
     {
+        internal static double itRatio;
         /// <summary>
         /// 将对应的Heatmap的值转换成canvas的实际坐标，
         /// result[i,0]和result[0,j]
@@ -19,7 +21,7 @@ namespace Canvas
         /// <param name="width"></param>
         /// <param name="height"></param>
         /// <returns></returns>
-        public static double[,] TransXY(List<int> x, List<int> y,double width , double height)
+        public static double[,] TransXY(List<int> x, List<int> y,Canvas canvas)
         {
             
             int _M = x.Count;
@@ -27,28 +29,32 @@ namespace Canvas
             double[,] _point = new double[_M+1,_N+1];       
             double[] canvasX = new double[_M];
             double[] canvasY = new double[_N];
+            double height = canvas.ActualHeight;
+            double width = canvas.ActualWidth;
             for (int i = 1; i < _M+1; i++)
             {
                 _point[i,0] = (width / _M) * x[i];
             }
             for (int j = 1; j < _N+1; j++)
             {
-                _point[0,j] =width- (width / _N) * y[j];
+                _point[0,j] =height- (height / _N) * y[j];
             }
             return _point;
 
         }
 
-        public static int InIt(double[,] spot,List<Point> polygon)
+        /// <summary>
+        /// 求出所在多边形内的数据数和整体数据数的比值
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="polygon"></param>
+        /// <returns></returns>
+        public static double RatioIn(double[] data, List<Point> polygon)
         {
-            int result=0;
-            double[,] _spot = spot;
-            List<Point> _point = polygon;
-            
+            itRatio = 0;
+            //TODO：将polygon转换成对应的坐标中的点，然后求出在其中的数据数目！！换个转换思路
 
-
-            return result;
+            return itRatio;
         }
-
     }
 }
